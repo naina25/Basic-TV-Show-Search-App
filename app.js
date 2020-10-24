0ffd7eaa3482f3fe41b2e5e66150620fa05afe98
naina <nanu02d8@gmail.com>
2020-10-24 22:25:53 +0530

    Added API and fetched required data(images)

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const form = document.querySelector(".searchForm");
-form.addEventListener("submit", (e)=>{
+const imageDiv = document.querySelector(".imageDiv");
+
+form.addEventListener("submit", async function(e){
     e.preventDefault();
     const searchItem = form.elements.query.value;
+    const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchItem}`);
+    const data = response.data;
+    for(let i=0 ; i < data.length-1; i++){
+        const image = data[i].show.image.medium;
+        const newImage = document.createElement("img");
+        newImage.src = image;
+        document.body.append(newImage);
+    }
 })
